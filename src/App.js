40550e175a5e64958e1e90ffb6dd84d8f4ef3d2d
20120602 <nmt762cva@gmail.com>
2023-10-30 21:30:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchText, setSearchText] = useState("");
+    const [searchedText, setSearchedText] = useState("");
+    const [photos, setPhotos] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const loader = useRef(null);
+
+    useEffect(() => {
+        if (searchedText) {
+            fetchPhotos();
+        }
+    }, [searchedText, page]);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 0.1,
+        };
+        const observer = new IntersectionObserver(handleObserver, options);
+        if (loader.current) {
+            observer.observe(loader.current);
+        }
+    }, []);
+
+    const handleObserver = (entities) => {
+        const target = entities[0];
+        if (target.isIntersecting && !loading) {
+            setPage((prevPage) => prevPage + 1);
+        }
+    };
+
+    const fetchPhotos = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(
+                `https://api.unsplash.com/search/photos?query=${searchedText}&page=${page}&per_page=10&client_id=OcdxbNRffDc0vKpHF3M5Q01bzbuzbtEXWx5G0XHkDlk`
+            );
+            const data = await response.json();
+            setPhotos((prevPhotos) => [...prevPhotos, ...data.results]);
+        } catch (error) {
+            console.error("Error fetching photos:", error);
+        } finally {
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        }
+    };
+
+    const handleSearch = () => {
+        setPhotos([]);
+        setPage(1);
+        setSearchedText(searchText);
+        setLoading(true);
+    };
+
+    return (
+        <div className="App">
+            <h1>Search Some Awesome Photos!</h1>
+            <div className="search-container">
+                <input
+                    type="text"
+                    placeholder="Search for photos..."
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    className="search-input"
+                />
+                <button onClick={handleSearch} className="search-button">
+                    Search
+                </button>
+            </div>
+            {loading && (
+                <div className="full-screen-loader">
+                    <div className="loader"></div>
+                </div>
+            )}
+            <div className="photo-list">
+                {photos.map((photo, index) => (
+                    <div key={photo.id} className="photo-item">
+                        <img src={photo.urls.small} alt={photo.alt_description} />
+                    </div>
+                ))}
+            </div>
+            <div ref={loader} className="loading-indicator">
+                {loading && (
+                    <div className="full-screen-loader">
+                        <div className="loader"></div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
